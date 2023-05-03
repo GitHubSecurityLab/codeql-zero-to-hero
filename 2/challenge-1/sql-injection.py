db4ef0f82184807fb60a328610e99b99abac8328
sylwia-budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2023-05-03 22:32:57 +0200

    Update sql-injection.py

--- 2/challenge-1/sql-injection.py
+++ 2/challenge-1/sql-injection.py
@@ -5,7 +5,7 @@ from django.db import connection
 def show_user(request, username):
     with connection.cursor() as cursor:
         # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+        cursor.execute("SELECT * FROM users WHERE username = %s" % username)
         user = cursor.fetchone()
 
         # GOOD -- Using parameters
@@ -13,7 +13,7 @@ def show_user(request, username):
         user = cursor.fetchone()
 
         # BAD -- Manually quoting placeholder (%s)
-        cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
+        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
         user = cursor.fetchone()
 
 urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
