d59ccc64cedf026a810d71ddcfe611c16971443a
sylwia-budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2023-05-03 23:23:16 +0200

    Update command-injection.py

--- 2/challenge-1/command-injection.py
+++ 2/challenge-1/command-injection.py
@@ -1,3 +1,9 @@
+urlpatterns = [
+    # Route to command_execution
+    url(r'^command-ex1$', command_execution_unsafe, name='command-execution-unsafe'),
+    url(r'^command-ex2$', command_execution_safe, name='command-execution-safe')
+]
+
 COMMANDS = {
     "list" :"ls",
     "stat" : "stat"
